595be5a6306ca9954f239a2f622bc2b476370ef5
James Heggs <james.heggs@techreturners.com>
2021-10-03 16:07:55 +0100

    Include extra unit tests and passing lengthy code for refactoring

--- src/transactions/transaction_processor.js
+++ src/transactions/transaction_processor.js
@@ -2,16 +2,42 @@ var txr = [];
 
 function processTransactions(transActions) {
 
+    txr = [];
+
     if(!validateTransactions(transActions)) {
         throw new Error("Undefined collection of transactions")
     }
 
-    const numberOfTransactions = transActions.length;
-    for(var i = 0; i < numberOfTransactions; i++) {
+    let txCount = {}
 
+    const numberOfTransactions = transActions.length;
+
+    for(var i = 0; i < numberOfTransactions; i++) {
+        const transaction = transActions[i];
+        txCount[transaction] ? txCount[transaction] += 1 : txCount[transaction] = 1;
     }
 
+    txCount = sortByAmountThenName(txCount);
+    
+    // Place them back in array for returning
+    Object.keys(txCount).forEach(function (key, index) {
+        txr[index] = `${key} ${txCount[key]}`;
+    });
+
     return txr;
+}
+
+function sortByAmountThenName(txCount) {
+    let sortedKeys = Object.keys(txCount).sort(function sortingFunction(itemOne, itemTwo) {
+        return  txCount[itemTwo] - txCount[itemOne] || itemOne > itemTwo || -(itemOne < itemTwo)}
+    );
+
+    let sortedResults = {};
+    for(let objectKey of sortedKeys) {
+        sortedResults[objectKey] = txCount[objectKey];
+    }
+
+    return sortedResults;
 }
 
 
--- src/transactions/transaction_processor.test.js
+++ src/transactions/transaction_processor.test.js
@@ -7,3 +7,23 @@ test('check transaction processing when empty', () => {
 test('check transaction processing exception thrown when null passed', () => {
     expect(() => processTransactions(null)).toThrow();
 });
+
+test('check single transaction', () => {
+    expect(processTransactions(['jumper'])).toEqual(['jumper 1']);
+});
+
+test('check multiple transactions same item', () => {
+    expect(processTransactions(['jumper', 'jumper'])).toEqual(['jumper 2']);
+});
+
+test('check multiple transactions multiple items', () => {
+    expect(processTransactions(['jumper', 'jeans', 'jeans'])).toEqual(['jeans 2', 'jumper 1']);
+});
+
+test('check multiple transactions alphabetical ordering correct', () => {
+    expect(processTransactions(['jumper', 'jeans'])).toEqual(['jeans 1', 'jumper 1']);
+});
+
+test('check multiple transactions numerical order correct', () => {
+    expect(processTransactions(['hoody', 'jeans', 'jeans'])).toEqual(['jeans 2', 'hoody 1']);
+});
