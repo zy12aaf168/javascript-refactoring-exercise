abeb9449a087d17ef6868743e52485afab0a3ffd
Zulfu <zulfu.yilmaz.a@gmail.com>
2023-06-01 13:14:04 +0100

    refactored with the group

--- src/transactions/transaction_processor.js
+++ src/transactions/transaction_processor.js
@@ -1,24 +1,13 @@
 export function processTransactions(transActions) {
-    if(!validateTransactions(transActions)) {
-        throw new Error("Undefined collection of transactions")
-    }
-
-    let txCount = transActions.reduce((a,t) => {
-        a[t] ? a[t] += 1 : a[t] = 1;
-        return a;
-    },{});
-
+    if (!validateTransactions(transActions)) throw new Error('Undefined collection of transactions');
+    const txCount = transActions.reduce((acc, transaction) => {
+        acc[transaction] ? (acc[transaction] += 1) : (acc[transaction] = 1);
+        return acc;
+    }, {});
     return sortByAmountThenName(txCount);
 }
-
 function sortByAmountThenName(txCount) {
-    let sortedKeys = Object.entries(txCount).sort(([itemOneKey, itemOneVal],[itemTwoKey,itemTwoValue]) => {
-        return  itemTwoValue - itemOneVal || itemOneKey > itemTwoKey || -(itemOneKey < itemTwoKey)}
-    );
-    
-    return sortedKeys.map(([key,value]) => `${key} ${value}`);
+    const sortedKeys = Object.entries(txCount).sort(([keyOne, valOne], [keyTwo, valTwo]) => valTwo - valOne || keyOne > keyTwo || -(keyOne < keyTwo));
+    return sortedKeys.map(([key, value]) => `${key} ${value}`);
 }
-
-function validateTransactions(transactions) {
-    return transactions !== undefined;
-}
+const validateTransactions = (transActions) => transActions !== undefined;
