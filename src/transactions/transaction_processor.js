73e9c5b33990c1d5725d8e4c419337db161fb205
Zulfu <zulfu.yilmaz.a@gmail.com>
2023-06-01 14:46:49 +0100

    Added comments for the methods for the rationale behind refactoring the bits of code

--- src/transactions/transaction_processor.js
+++ src/transactions/transaction_processor.js
@@ -1,13 +1,24 @@
 export function processTransactions(transActions) {
     if (!validateTransactions(transActions)) throw new Error('Undefined collection of transactions');
+
+    //we use reduce instead of foreach, and we start it off with an initial value of an empty object and that value is populated and returned on each run.
     const txCount = transActions.reduce((acc, transaction) => {
         acc[transaction] ? (acc[transaction] += 1) : (acc[transaction] = 1);
         return acc;
     }, {});
     return sortByAmountThenName(txCount);
 }
+
 function sortByAmountThenName(txCount) {
-    const sortedKeys = Object.entries(txCount).sort(([keyOne, valOne], [keyTwo, valTwo]) => valTwo - valOne || keyOne > keyTwo || -(keyOne < keyTwo));
-    return sortedKeys.map(([key, value]) => `${key} ${value}`);
+    /* We use the entries method instead of keys, to get a tuple of k,v pairings to perform a sort on, so that we have the key-values in the same place rather 
+    than having to refer back to the initial array to pair them up again    
+    */       
+    const sortedEntries = Object.entries(txCount).sort(([keyOne, valOne], [keyTwo, valTwo]) => valTwo - valOne || keyOne > keyTwo || -(keyOne < keyTwo));
+
+    /* Because sorted entries is an array of tuples of <string, any>, we need to map it so that it fits what the test cases are expecting which is an Array of string
+    Throughout both of these methods, we make use of destructuring assignments, to retrieve the values of the tuples.
+    */
+    return sortedEntries.map(([key, value]) => `${key} ${value}`);
 }
+
 const validateTransactions = (transActions) => transActions !== undefined;
